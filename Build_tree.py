93038118801cc75198b7a9d3538b906e5023bd6d
uzun-masha <87938458+uzun-masha@users.noreply.github.com>
2021-07-25 17:07:17 +0300

    Update Build_tree.py

--- Build_tree.py
+++ Build_tree.py
@@ -13,7 +13,7 @@ def cdhit_meta(db,out):
     #out='/mnt/c/Users/sukha/mamK_cd_hit'
     os.system(f'cd-hit -i {db} -c 0.7 -o {out}')
     os.system(f"mafft {out} > al_{out}")
-    os.system(f"iqtree -s al_{out} -m TEST -bb 1000 -nt 4")
+    os.system(f"iqtree -s al_{out} -m TEST -bb 1000 -nt 16")
 cdhit_meta(db,out)
 
 # pipeline.py input.fasta
